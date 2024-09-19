4a405820cb5d3b8b387e9ac416865d57619b24b6
ZAMBAR <zambar@163.com>
2024-09-19 21:10:59 +0800

    init: mai-random page

---
+++ random-tool/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Card, CardContent, CardHeader, Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <Container maxWidth="sm">
+        <Card>
+          <CardHeader title="Random Tool" />
+          <CardContent>
+            <Typography variant="body1">Random Tool</Typography>
+          </CardContent>
+        </Card>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
